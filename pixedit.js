e7e48961a572f6f87afa30aa0ae33d779b19d59b
Luke Palmer <lrpalmer@gmail.com>
2018-04-23 18:11:13 -0400

    Basic save/load to array

--- pixedit.js
+++ pixedit.js
@@ -16,7 +16,9 @@ cx.fillStyle = '#000000';
 
 var touched = new Int8Array(PARAMS.width * PARAMS.height);
 
-var modscounter = $('<div>').text("0/" + PARAMS.modlimit);
+var modscounter = $('<span>').css('font-size', '30px')
+                             .css('font-family', 'monospace')
+                             .text("0/" + PARAMS.modlimit);
 var mods = 0;
 
 
@@ -100,7 +102,7 @@ var handletouch = function(e) {
   }
 };
 
-var palette = $('<div>');
+var palette = $('<span>');
 var paletteColors = [ 
   "#000000",
   "#0000ff",
@@ -110,6 +112,7 @@ var paletteColors = [
   "#ff00ff",
   "#ffff00",
   "#ffffff" ];
+var selectedColor = null;
 for (var i = 0; i < paletteColors.length; i++) {
   (function() {
     var color = paletteColors[i];
@@ -117,13 +120,21 @@ for (var i = 0; i < paletteColors.length; i++) {
                            .css('width', '50px')
                            .css('height', '50px')
                            .css('border', '1px solid black')
+                           .css('margin', '5px')
                            .css('background-color', color);
+    if (selectedColor == null) { selectedColor = entry; }
     entry.click(function(e) {
       cx.fillStyle = color;
+      selectedColor.css('border', '1px solid black');
+      selectedColor.css('margin', '5px');
+      entry.css('border', '6px groove');
+      entry.css('margin', '0px');
+      selectedColor = entry;
     });
     palette.append(entry);
   })();
 }
+selectedColor.click();
 
 canvas.addEventListener('mousedown', handlemouse);
 canvas.addEventListener('mousemove', handlemouse);
@@ -134,8 +145,40 @@ canvas.addEventListener('touchend', handletouch);
 canvas.addEventListener('scroll', handlescroll);
 
 PARAMS.container.appendChild(canvas);
-PARAMS.container.appendChild(modscounter[0]);
-PARAMS.container.appendChild(palette[0]);
+PARAMS.container.appendChild($('<div>').append(
+  palette,
+  modscounter)[0]);
+
+$$.toArray = function() {
+  var data = Array(PARAMS.height * PARAMS.width * 4);
+  for (var y = 0; y < PARAMS.height; y++) {
+    for (var x = 0; x < PARAMS.width; x++) {
+      var imgdata = cx.getImageData(PARAMS.scale*x,PARAMS.scale*y,1,1);
+      var ix = 4*(PARAMS.width*y + x);
+      data[ix + 0] = imgdata.data[0];
+      data[ix + 1] = imgdata.data[1];
+      data[ix + 2] = imgdata.data[2];
+      data[ix + 3] = imgdata.data[3];
+    }
+  }
+  return data;
+};
+
+var rgbaToHex = function(r,g,b,a) {
+  return "#" + ("00000000" + ((r << 24) | (g << 16) | (b << 8) | a).toString(16)).slice(-8);
+};
+
+$$.loadArray = function(data) {
+  var localcx = canvas.getContext('2d');
+  localcx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var y = 0; y < PARAMS.height; y++) {
+    for (var x = 0; x < PARAMS.width; x++) {
+      var ix = 4*(PARAMS.width*y + x);
+      localcx.fillStyle = rgbaToHex(data[ix+0], data[ix+1], data[ix+2], data[ix+3]);
+      localcx.fillRect(x*PARAMS.scale, y*PARAMS.scale, PARAMS.scale, PARAMS.scale);
+    }
+  }
+};
 
 return $$;
 };
